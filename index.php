667d160350888136d471290f98909a2fb08db0c8
pawan <pawankumar.s@karmanya.co.in>
2016-04-19 15:04:10 +0530

    currency conversion

--- index.php
+++ index.php
@@ -6,10 +6,15 @@
     </head>
     <body>
         <form method="post">
+            <P>Use Currency Codes only</P>
             Amount: <input type="text" name="amount" required>
             From: <input type="text" name="from" required>
             To: <input type="text" name="to" required>
-            <input type="submit" value="Submit">
+            <input type="submit" value="Submit" name='convert'>
         </form>
+        <div><br><br><?php if(isset($data[0])){
+echo $data[0];}?></div>
+        <article>
+        </article>
     </body>
 </html>
--- submit.php
+++ submit.php
@@ -1,10 +1,10 @@
 <?php
-if(isset($_POST["amount"])){
+if(isset($_POST["convert"])){
 $Amount=$_POST['amount'];
 $From=$_POST['from'];
 $To=$_POST['to'];
 $result = file_get_contents('http://www.xe.com/currencyconverter/convert/?Amount='.$Amount.'&From='.$From.'&To='.$To);
 preg_match('/<td width="47%" align="left" class="rightCol">(.*?)<td width="6%" valign="middle" align="center" rowspan="2" class="invCol">/s',$result,$data);
-echo $data[0];
+
 }
 ?>
